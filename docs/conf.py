46a0ec8f4ed9a71d619c048d8b0feafeb2e68b08
arielzenprotocol <ariel@zenprotocol.com>
2018-10-15 17:29:40 +0300

    trying to change style

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,8 @@ html_theme = 'default'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_style = 'css/my_theme.css' 
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
